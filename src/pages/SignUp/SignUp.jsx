37953d075e1732eb2709d4910f30be1b4d9d9ce2
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-09-17 19:26:01 +0300

    add form Rg and Lg

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,17 +1,31 @@
-import { BackgroundImage, Button, Section, TextGrey, TitlePage } from 'components';
-import { LinkSignIn, Question, Wrapper } from './SignUp.styled';
+import { Section, SignUpForm, TextGrey, TitlePage } from 'components';
+import { LinkSignIn, PictureBg, Question, Wrapper } from './SignUp.styled';
+import bg from 'images/bg';
+const { bg1Desktop1x, bg1Desktop2x, bg1Mobile1x, bg1Mobile2x } = bg;
 
 export default function SingUp() {
   return (
     <Section>
       <TitlePage text="Sign Up" />
       <TextGrey text="Thank you for your interest in our platform. To complete the registration process, please provide us with the following information." />
-      <Button label="Sign Up" />
+      <SignUpForm />
       <Wrapper>
         <Question>Already have account?</Question>
         <LinkSignIn to="/login">Sign In</LinkSignIn>
       </Wrapper>
-      <BackgroundImage />
+      <PictureBg>
+        <source
+          type="image/jpg"
+          media="(max-width: 767px)"
+          srcSet={`${bg1Mobile1x} 1x, ${bg1Mobile2x} 2x`}
+        />
+        <source
+          type="image/jpg"
+          media="(min-width: 768px)"
+          srcSet={`${bg1Desktop1x} 1x, ${bg1Desktop2x} 2x`}
+        />
+        <img src={`${bg1Desktop1x}`} alt="Woman doing sports" />
+      </PictureBg>
     </Section>
   );
 }
