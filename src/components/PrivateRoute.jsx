2df0d2c40e9d455d6c9a2b93fb945e7dcb19e203
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-09-20 09:49:41 +0300

    add private and restricted route

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,1 +1,10 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn, selectIsRefreshing } from 'redux/auth/selectors';
 
+export const PrivateRoute = ({ redirectTo = '/' }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
+  const shouldRedirect = isLoggedIn && !isRefreshing;
+  return shouldRedirect ? <Outlet /> : <Navigate to={redirectTo} />;
+};
