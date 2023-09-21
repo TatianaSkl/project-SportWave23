7daefd39a5bfffd8043d859406a1a055df9328e1
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-09-21 20:43:13 +0300

    delete api

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -14,7 +14,7 @@ const clearAuthHeader = () => {
 
 export const register = createAsyncThunk('auth/register', async (credentials, thunkAPI) => {
   try {
-    const res = await axios.post('/api/auth/register', credentials);
+    const res = await axios.post('/auth/register', credentials);
     setAuthHeader(res.data.token);
     return res.data;
   } catch (error) {
@@ -25,7 +25,7 @@ export const register = createAsyncThunk('auth/register', async (credentials, th
 
 export const logIn = createAsyncThunk('auth/login', async (credentials, thunkAPI) => {
   try {
-    const res = await axios.post('/api/auth/login', credentials);
+    const res = await axios.post('/auth/login', credentials);
     setAuthHeader(res.data.token);
     return res.data;
   } catch (error) {
@@ -36,7 +36,7 @@ export const logIn = createAsyncThunk('auth/login', async (credentials, thunkAPI
 
 export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
   try {
-    await axios.post('/api/auth/logout');
+    await axios.post('/auth/logout');
     clearAuthHeader();
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
@@ -51,7 +51,7 @@ export const refreshUser = createAsyncThunk('auth/refresh', async (_, thunkAPI) 
   }
   try {
     setAuthHeader(persistedToken);
-    const res = await axios.get('/api/auth/current');
+    const res = await axios.get('/auth/current');
     return res.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
@@ -63,7 +63,7 @@ export const updateAvatar = createAsyncThunk('auth/avatar', async (file, thunkAP
     const formData = new FormData();
     formData.append('avatar', formData);
 
-    const res = await axios.patch('/api/auth/avatars', formData, {
+    const res = await axios.patch('/auth/avatars', formData, {
       headers: { 'content-type': 'multipart/form-data' },
     });
 
