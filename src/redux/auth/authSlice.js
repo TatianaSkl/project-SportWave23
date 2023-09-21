db6be58fe7c79caffc92ee62b2f13bf4338d4043
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-09-21 16:01:08 +0300

    add initialValues

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -2,7 +2,12 @@ import { createSlice } from '@reduxjs/toolkit';
 import { register, logIn, logOut, refreshUser } from './operations';
 
 const initialState = {
-  user: { name: null, email: null },
+  user: {
+    name: null,
+    email: null,
+    userParams: {},
+    avatarURL: null,
+  },
   token: null,
   isLoggedIn: false,
   isRefreshing: false,
