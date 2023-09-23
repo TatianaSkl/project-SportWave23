19f146c70b645617f52c119e127ec961f0fd406b
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-09-23 11:41:31 +0300

    fix

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,58 +1,55 @@
 import style from './Products.module.css';
-// import axios from 'axios';
+import axios from 'axios';
 import ProductsFilters from '../../components/ProductsFilters/ProductsFilters';
 import ProductsList from '../../components/ProductsList/ProductsList';
-// import { useEffect, useState } from 'react';
-// import { useSelector } from 'react-redux';
-// import { selectUser } from 'redux/auth/selectors';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from 'redux/auth/selectors';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default function Products() {
-
   const [searchValue, setSearchValue] = useState('');
   const [categoryProduct, setCategoryProduct] = useState('');
   const [typeProduct, setTypeProduct] = useState('all');
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(false);
-  
+
   const user = useSelector(selectUser);
   const TOKEN = user.token;
-  
+
   axios.defaults.baseURL = 'https://power-pulse-project-backend.onrender.com';
 
- useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
       try {
         const config = {
           method: 'get',
           maxBodyLength: Infinity,
           url: `/products?limit=10&page=${page}&search=${searchValue}&category=${categoryProduct}&recommended=${typeProduct}`,
-          headers: { 
-            'Authorization': `Bearer ${TOKEN}`
-          }
+          headers: {
+            Authorization: `Bearer ${TOKEN}`,
+          },
         };
-        
+
         const response = await axios.request(config);
         const data = response.data;
-        
+
         if (data.result.length >= 10) {
           setHasMore(true);
         }
         if (page === 1) {
           setProducts(data.result);
         } else {
-          setProducts((prevProducts) => [
-            ...prevProducts, ...data.result]);
+          setProducts(prevProducts => [...prevProducts, ...data.result]);
         }
-        
       } catch (error) {
         console.error(error);
       }
     };
     fetchData();
- }, [TOKEN, searchValue, categoryProduct, page, typeProduct]);
-  
+  }, [TOKEN, searchValue, categoryProduct, page, typeProduct]);
+
   const fetchMoreData = () => {
     setPage(page + 1);
   };
@@ -61,20 +58,18 @@ export default function Products() {
     <>
       <h1 className={style.title}>Products</h1>
       <ProductsFilters
-      // setArrayProducts={setProducts}
-      // setValue={setSearchValue}
-      // setCategory={setCategoryProduct}
-      // setType={setTypeProduct}
+        setArrayProducts={setProducts}
+        setValue={setSearchValue}
+        setCategory={setCategoryProduct}
+        setType={setTypeProduct}
       />
       <InfiniteScroll
-      // dataLength={products.length}
-      // next={fetchMoreData}
-      // hasMore={hasMore}
-      // loader={''}
+        dataLength={products.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        // loader={''}
       >
-        <ProductsList
-        // productsArray={products}
-        />
+        <ProductsList productsArray={products} />
       </InfiniteScroll>
     </>
   );
