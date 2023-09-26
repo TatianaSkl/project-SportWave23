6c21b0dd2969cc32b68d3ea9338dc32116c4c280
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-09-26 04:07:20 +0000

    Deploying to gh-pages from @ TatianaSkl/project-SportWave23@c1cce6c2796d5ed870aa2a06ad65a630eb297e30 🚀

---
+++ static/js/291.8095061a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[291],{1291:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var r=n(7805),c=n(9016),u=n(3329);function a(){return(0,u.jsx)(r.Section,{children:(0,u.jsx)(c.Z,{})})}}}]);
+//# sourceMappingURL=291.8095061a.chunk.js.map
