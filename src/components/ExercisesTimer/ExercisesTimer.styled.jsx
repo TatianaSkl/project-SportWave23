acb929b1baf45e919a7d39713d052288f82ed4d0
VadymMiezin <vadym.miezin@gmail.com>
2023-09-29 01:01:07 +0300

    add logic for add to diary button

--- src/components/ExercisesTimer/ExercisesTimer.styled.jsx
+++ src/components/ExercisesTimer/ExercisesTimer.styled.jsx
@@ -1,63 +1,75 @@
 import styled from '@emotion/styled';
 
-const TimerWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+export const TimeSpinner = styled.div`
+  width: 125px;
+  margin-top: 4px;
+  margin-bottom: 14px;
+  height: 125px;
+  display: inherit;
+  border-radius: 50%;
+  justify-content: center;
   align-items: center;
+  border: 1px solid rgba(239, 237, 232, 0.1);
+`;
+export const SpinnerWrap = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
   position: relative;
 `;
-const TimerTitle = styled.h1`
-  color: rgba(239, 237, 232, 0.4);
-  font-size: 10px;
-  font-weight: 400;
-  line-height: 14px;
-  margin-bottom: 4px;
-`;
-
-const FormattedTitle = styled.p`
-  color: rgba(239, 237, 232, 0.4);
-  font-size: 10px;
-  font-weight: 400;
-  line-height: 14px;
-  margin-bottom: 4px;
-`;
-
-const TimerBtn = styled.button`
+export const TimePauseButton = styled.button`
+  width: 32px;
+  height: 32px;
   background-color: #e6533c;
-  margin-bottom: 8px;
-  padding: 10px;
-  border-radius: 5px;
+  color: #efede8;
+  border-radius: 4px;
+  border: none;
   display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 8px;
 `;
-
-const PlayIcon = styled.svg`
-  width: 10px;
-  height: 10px;
-`;
-
-const TimerText = styled.p`
-  color: rgba(239, 237, 232, 0.3);
+export const TimeLeft = styled.span`
+  color: #efede8;
   font-family: Roboto;
-  font-size: 14px;
-  font-weight: 400;
-  line-height: 18px;
-  margin-bottom: 40px;
+  font-size: 16px;
+  line-height: calc(24px / 16px);
+  position: absolute;
+  top: 65px;
 `;
-
-const TimerSub = styled.span`
+export const TimeText = styled.p`
+  text-align: center;
+  color: rgba(239, 237, 232, 0.4);
+  font-family: 'Roboto';
+  font-size: 10px;
+  line-height: calc(14px / 10px);
+`;
+export const Calories = styled.p`
+  color: rgba(239, 237, 232, 0.3);
+  font-family: 'Roboto';
+  font-size: 14px;
+  text-align: center;
+  line-height: calc(18px / 14px);
+`;
+export const CaloriesValue = styled.span`
   color: #e6533c;
-  font-size: 14px;
-  font-weight: 400;
-  line-height: 18px;
-  display: inline-flex;
+`;
+export const CircleIcon = styled.svg`
+  rotate: 90deg;
+  height: 125px;
+  width: 125px;
+  margin-bottom: 14px;
 `;
 
-export {
-  TimerWrapper,
-  TimerTitle,
-  FormattedTitle,
-  TimerBtn,
-  PlayIcon,
-  TimerText,
-  TimerSub,
-};
+export const InactiveCircle = styled.circle`
+  fill: transparent;
+  stroke-width: 4;
+  stroke: rgba(239, 237, 232, 0.1);
+`;
+
+export const ActiveCircle = styled.circle`
+  fill: transparent;
+  stroke-width: 4;
+  stroke: #e6533c;
+  transition: stroke-dashoffset 0.25s linear;
+`;
