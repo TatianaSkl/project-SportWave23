3574a75c90292699c840c89779ac578cb8e6572e
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-10-01 21:35:49 +0300

    fix route

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -2,9 +2,8 @@ import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from 'redux/auth/selectors';
 
-export const RestrictedRoute = ({ component: Component, restricted = false, redirectTo = '/' }) => {
+export const RestrictedRoute = ({ component, redirectTo }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
-  const shouldRedirect = isLoggedIn && restricted;
 
-  return shouldRedirect ? <Navigate to={redirectTo} /> : <Component />;
+  return isLoggedIn ? <Navigate to={redirectTo} /> : component;
 };
